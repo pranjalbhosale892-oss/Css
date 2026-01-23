a689839bff3b325005dab0cd19c6e44c650f2f4b
pranjalbhosale892-oss <pranjalbhosale892@gmail.com>
2026-01-23 10:56:38 +0000

    update

---
+++ css_app/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  name="bhosale"
+  protected readonly title = signal('css_app');
+}
